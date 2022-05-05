7211e2181ae82e9a08bcef937fc774cd0bb6bdd9
grfoger <grfoger@gmail.com>
2022-05-05 09:09:08 +0300

    3. PostController.createPost. Создание вакансии. [#504849] - add counter

--- src/main/java/ru/job4j/dreamjob/store/PostStore.java
+++ src/main/java/ru/job4j/dreamjob/store/PostStore.java
@@ -6,17 +6,20 @@ import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PostStore {
 
     private static final PostStore INST = new PostStore();
 
+    private AtomicInteger idCount = new AtomicInteger(1);
+
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
     private PostStore() {
-        posts.put(1, new Post(1, "Junior Java Job", "Вакансия:Младший Java разработчик", LocalDate.now()));
-        posts.put(2, new Post(2, "Middle Java Job", "Вакансия:Java разработчик", LocalDate.now()));
-        posts.put(3, new Post(3, "Senior Java Job", "Вакансия:Старший Java разработчик", LocalDate.now()));
+        posts.put(idCount.getAndIncrement(), new Post(1, "Junior Java Job", "Вакансия:Младший Java разработчик", LocalDate.now()));
+        posts.put(idCount.getAndIncrement(), new Post(2, "Middle Java Job", "Вакансия:Java разработчик", LocalDate.now()));
+        posts.put(idCount.getAndIncrement(), new Post(3, "Senior Java Job", "Вакансия:Старший Java разработчик", LocalDate.now()));
     }
 
     public static PostStore instOf() {
@@ -28,6 +31,6 @@ public class PostStore {
     }
 
     public void add(Post post) {
-        posts.put(post.getId(), post);
+        posts.put(idCount.getAndIncrement(), post);
     }
 }
